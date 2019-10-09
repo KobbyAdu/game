51d56364e627d22a8348c72efaf3dfc3fa029640
Kobby Adu <kobby129@gmail.com>
2019-10-09 10:58:30 -0600

    Player model Move

---
+++ gosu.rb
@@ -0,0 +1,81 @@
+require 'gosu'
+
+class Tutorial < Gosu::Window # creates a window which the game will be played on
+  def initialize
+    super 640, 480
+    self.caption = "Tutorial Game"
+
+    @background_image = Gosu::Image.new("space.png", :tileable => true) # takes two arguments to make background
+
+    @player = Player.new
+    @player.warp(320, 240)
+  end
+  
+  def update
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+        @player.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+        @player.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+        @player.accelerate
+    end
+    @player.move
+  end
+  
+  def draw
+    @player.draw
+    @background_image.draw(0,0,0)
+  end
+  def button_down(id)
+
+    if id == Gosu::KB_ESCAPE
+        close
+    else
+        super
+    end
+   end
+end
+
+# these set like framerate i think
+class Player
+    def initialize
+        @image = Gosu::Image.new("starfighter.bmp")
+        @x = @y = @vel_x = @vel_y = @angle = 0.0
+        @score = 0
+    end
+
+    def warp(x,y)
+        @x, @y = x, y 
+    end
+    def turn_right
+        @angle += 4.5
+    end
+
+    def turn_left
+        @angle -= 4.5
+    end
+
+    def accelerate
+        @vel_x += Gosu.offset_x(@angle, 0.5)
+        @vel_y += Gosu.offset_y(@angle, 0.5)
+    end
+
+    def move
+        @x += @vel_x 
+        @y += @vel_y
+        @x %= 640
+        @y %= 480
+        @vel_x *= 0.95
+        @vel_y *= 0.95
+    end
+    
+    def draw
+        @image.draw_rot(@x, @y, 1, @angle)
+    end
+Tutorial.new.show # causes window to be shown
+
+
+end
+
